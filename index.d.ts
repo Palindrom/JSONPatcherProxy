f0259e7d94b467a8e7852429ff30134368f4a404
Omar Alshaker <omar@omaralshaker.com>
2017-02-23 11:05:37 +0100

    Fix Types and JSDoc

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,48 @@
 /** Class representing a JS Object observer  */
 declare class JSONPatcherProxy {
-    private static deepClone(obj);
+    /**
+    * Deep clones your object and returns a new object.
+    */
+    public static deepClone(obj);
     private originalObject;
     private exposedObject;
     private cachedProxy;
     private patches;
     private isRecording;
     private defaultCallback;
-    userCallback: Function;
+    private userCallback: Function;
     /**
     * Disables recording and/or callback firing when object modifications happen.
-    * @param {boolean} enable - true will enable, false will disable.
     */
-    switchObserverOff: Function;
+    public switchObserverOff: Function;
     /**
     * Enables recording and/or callback firing when object modifications happen.
-    * @param {boolean} enable - true will enable, false will disable.
     */
-    switchObserverOn: Function;
+    public switchObserverOn: Function;
     private static escapePathComponent(str);
     private generateProxyAtPath(obj, path);
     private _proxifyObjectTreeRecursively(root, path);
     private proxifyObjectTree(root);
+    /**
+    * Creates an instance of JSONPatcherProxy around your object of interest, for later observe, unobserve, switchCallbackOff, switchCallbackOn calls. 
+    * @param {Object|Array} root - the object you want to wrap
+    * @returns {JSONPatcherProxy}
+    * @constructor
+    */
     constructor(root: any);
     /**
      * Proxifies the object that was passed in the constructor and returns a proxified mirror of it.
-     * @param {boolean} record - whether to record object changes to a later-retrievable patches array.
-     * @param {function} [callback] - this will be synchronously called with every object change.
+     * @param {Boolean} record - whether to record object changes to a later-retrievable patches array.
+     * @param {Function} [callback] - this will be synchronously called with every object change.
      */
-    observe(record: any, callback: any): any;
+    public observe(record: any, callback: any): any;
     /**
      * If the observed is set to record, it will synchronously return all the patches and empties patches array.
      */
-    generate(): Object[];
+    public generate(): Object[];
     /**
      * Synchronously de-proxifies the last state of the object and returns it unobserved.
      */
-    unobserve(): any;
+    public unobserve(): any;
 }
 export default JSONPatcherProxy;
