6a67ac0f85f3dea00582336883eb58b12728707c
Omar Alshaker <omar@omaralshaker.com>
2017-11-08 10:49:52 +0100

    Minify dist file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
-var webpack = require("webpack");
+const webpack = require('webpack');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = [
   {
-    entry: "./src/jsonpatcherproxy.js",
+    entry: './src/jsonpatcherproxy.js',
     output: {
-      filename: "dist/jsonpatcherproxy.min.js",
-      library: "JSONPatcherProxy",
-      libraryTarget: "var"
+      filename: 'dist/jsonpatcherproxy.min.js',
+      library: 'JSONPatcherProxy',
+      libraryTarget: 'var'
     },
+    plugins: [
+      new MinifyPlugin(),
+      // src file can be used in production everywhere
+      new CopyWebpackPlugin([
+        { from: './src/jsonpatcherproxy.js', to: './dist/jsonpatcherproxy.js' }
+      ])
+    ],
     resolve: {
-      extensions: [".js"]
+      extensions: ['.js']
     }
   }
 ];
